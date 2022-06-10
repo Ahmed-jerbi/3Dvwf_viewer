4abb45fa927188d3e51cbcc68ecf62db399334aa
Ahmed Jerbi <52385138+Ahmed-jerbi@users.noreply.github.com>
2022-06-10 14:23:16 +0200

    first commit

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using SimpleFileBrowser;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	public GameObject UIButtoApply;
+	public GameObject UIPanelLoading;
+	public Text UITextPath;
+	string vwf_path;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		//Init UI
+		
+		UIPanelLoading.SetActive(false);
+		
+		// Set plguin VWF path: Check if in BUILD (.exe) or in DEV (Unity Editor)
+		if (Application.platform == RuntimePlatform.WindowsEditor)
+			 vwf_path = Application.dataPath + @"\Plugins\VIOSO\vioso.vwf"; 
+		else vwf_path = Application.dataPath + @"\Plugins\x86_64\vioso.vwf";
+
+		//Set File Browser Filters
+		FileBrowser.SetFilters(true, new FileBrowser.Filter("Calib", ".vwf"));
+		FileBrowser.SetDefaultFilter(".vwf");
+	}
+
+	//Called by Load Button 
+	public void ButtonSelect()
+	{
+		FileBrowser.ShowLoadDialog((path) => { SelectVwf(path); }, null, FileBrowser.PickMode.Files, false, null,null, "Select VWF", "Load");
+	}
+
+	//  File Selected method
+    void SelectVwf(string[] path)
+    {
+		Debug.Log("VWF loaded: "+ FileBrowser.Result[0]);
+		UIButtoApply.GetComponent<Button>().interactable = true;
+		UITextPath.text = Path.GetFileName(FileBrowser.Result[0]);
+	}
+
+    //Called by Apply Button 
+    public void ButtonApply()
+    {
+		StartCoroutine(ApplyVWF());
+    }
+
+	// Coroutine to make sure loading screen starts before operations
+	IEnumerator ApplyVWF()
+    {
+		UIPanelLoading.SetActive(true);
+		yield return new WaitForSeconds(0.5f);
+		//Copy vwf to plugin path
+		File.Copy(FileBrowser.Result[0], vwf_path, true);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
